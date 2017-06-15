9249495a4b2e2468ba0105a58604fdf4b8214bde
nelyflores <nely.afloresr@gmail.com>
2017-06-15 16:05:04 +0000

    realiza busqueda del tema y la imprime

--- js/main.js
+++ js/main.js
@@ -6,8 +6,11 @@ console.log($tasksList)
 var cargarPagina = function () {
   cargarTemas();
   var saber=$("#agregar_formulario");
+  var boton=$("#botonBusqueda");
+  console.log(boton);
    $("#agregar_formulario").submit(agregarTema);
-console.log(saber);
+   $("#botonBusqueda").click(filtrarContactos);
+
   
 };
 var cargarTemas = function () {
@@ -18,12 +21,8 @@ var cargarTemas = function () {
 }
 var crearTemas = function (tema) {
   var temas = tema.content;
-  console.log(temas);
   var autor = tema.author_name;
   var cuantasRespuestas = tema.responses_count;
-  
-
-
   var plantillaNueva = plantilla.replace("__titulo-tema__", temas).replace("__autor__",autor).replace("__numRespuestas__",cuantasRespuestas);
   $tasksList.append(plantillaNueva);
 
@@ -50,6 +49,32 @@ var plantilla = '<tr>' +
                     '<td>__autor__</td>' +
                     '<td>__numRespuestas__</td>' +
                   '</tr>';
+
+var filtrarContactos= function(e){
+	e.preventDefault();
+	var criterioBusqueda = $("#palabra-buscar").val().toLowerCase();
+	alert("la palabra a buscar es " + criterioBusqueda);
+	$.getJSON(api.url, function (temas) {
+    var filtrarTema= temas.filter(function(tarea){
+    	return tarea.content.toLowerCase().indexOf(criterioBusqueda) >= 0;
+    });
+    console.log(filtrarTema);
+    mostrarContactos(filtrarTema);
+	});
+	
+
+	
+};
+var mostrarContactos = function (temas) {
+	var plantillaFinal = "";
+	temas.forEach(function (tema) {
+		plantillaFinal += plantilla.replace("__titulo-tema__", tema.content)
+			.replace("__autor__", tema.author_name)
+			.replace("__numRespuestas__", tema.responses_count);
+	});
+	$("#listaTemas").html(plantillaFinal);
+};
+
 
 
 
