cd7cee0d90c3e6c4de493d80c4d6e2e27e3f7531
nelyflores <nely.afloresr@gmail.com>
2017-06-15 16:48:53 +0000

    indentando codigo

--- js/main.js
+++ js/main.js
@@ -1,65 +1,65 @@
 var api = {
-  url: "http://examen-laboratoria-sprint-5.herokuapp.com/topics"
+	url: "http://examen-laboratoria-sprint-5.herokuapp.com/topics"
 };
 var $tasksList = $("#listaTemas");
 console.log($tasksList)
 var cargarPagina = function () {
-  cargarTemas();
-  var saber=$("#agregar_formulario");
-  var boton=$("#botonBusqueda");
-  console.log(boton);
-   $("#agregar_formulario").submit(agregarTema);
-   $("#botonBusqueda").click(filtrarContactos);
+	cargarTemas();
+	var saber=$("#agregar_formulario");
+	var boton=$("#botonBusqueda");
+	console.log(boton);
+	$("#agregar_formulario").submit(agregarTema);
+	$("#botonBusqueda").click(filtrarContactos);
 
-  
+	
 };
 var cargarTemas = function () {
-  $.getJSON(api.url, function (temas) {
-    temas.forEach(crearTemas );
+	$.getJSON(api.url, function (temas) {
+		temas.forEach(crearTemas );
 
-  });
+	});
 }
 var crearTemas = function (tema) {
-  var temas = tema.content;
-  var autor = tema.author_name;
-  var cuantasRespuestas = tema.responses_count;
-  var plantillaNueva = plantilla.replace("__titulo-tema__", temas).replace("__autor__",autor).replace("__numRespuestas__",cuantasRespuestas);
-  $tasksList.append(plantillaNueva);
+	var temas = tema.content;
+	var autor = tema.author_name;
+	var cuantasRespuestas = tema.responses_count;
+	var plantillaNueva = plantilla.replace("__titulo-tema__", temas).replace("__autor__",autor).replace("__numRespuestas__",cuantasRespuestas);
+	$tasksList.append(plantillaNueva);
 
 };
 var agregarTema = function (e) {
- e.preventDefault();
-  var nombre = $("#nombre-autor").val();
-  var tituloTema = $("#titulo-tema").val()
- console.log(nombre);
- console.log(tituloTema);
- $.post(api.url, {
-    author_name: nombre,
-    content:tituloTema,
-    responses_count:0
-  }, function (tema) {
-    crearTemas(tema);
-    $("#myModal").modal("hide");
-  });
- 
+	e.preventDefault();
+	var nombre = $("#nombre-autor").val();
+	var tituloTema = $("#titulo-tema").val()
+	console.log(nombre);
+	console.log(tituloTema);
+	$.post(api.url, {
+		author_name: nombre,
+		content:tituloTema,
+		responses_count:0
+	}, function (tema) {
+		crearTemas(tema);
+		$("#myModal").modal("hide");
+	});
+	
 };
 
 var plantilla = '<tr>' +
-					'<td>__titulo-tema__</td>'+
-                    '<td>__autor__</td>' +
-                    '<td>__numRespuestas__</td>' +
-                  '</tr>';
+'<td>__titulo-tema__</td>'+
+'<td>__autor__</td>' +
+'<td>__numRespuestas__</td>' +
+'</tr>';
 
 var filtrarContactos= function(e){
 	e.preventDefault();
 	var criterioBusqueda = $("#palabra-buscar").val().toLowerCase();
 	alert("la palabra a buscar es " + criterioBusqueda);
 	$.getJSON(api.url, function (temas) {
-    var filtrarTema= temas.filter(function(tarea){
-    	return tarea.content.toLowerCase().indexOf(criterioBusqueda) >= 0;
-    });
-    console.log(filtrarTema);
-    mostrarContactos(filtrarTema);
+		var filtrarTema= temas.filter(function(tarea){
+			return tarea.content.toLowerCase().indexOf(criterioBusqueda) >= 0;
+		});
+		console.log(filtrarTema);
+		mostrarContactos(filtrarTema);
 	});
 	
 
@@ -69,13 +69,10 @@ var mostrarContactos = function (temas) {
 	var plantillaFinal = "";
 	temas.forEach(function (tema) {
 		plantillaFinal += plantilla.replace("__titulo-tema__", tema.content)
-			.replace("__autor__", tema.author_name)
-			.replace("__numRespuestas__", tema.responses_count);
+		.replace("__autor__", tema.author_name)
+		.replace("__numRespuestas__", tema.responses_count);
 	});
 	$("#listaTemas").html(plantillaFinal);
 };
-
-
-
 
 $(document).ready(cargarPagina);
